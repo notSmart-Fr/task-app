0d29bdfe9f6583258efcb9d4e2beee67ea314e2c
notSmart-Fr <yasirsamin4@gmail.com>
2026-08-11 10:08:58 +0600

    first commit after package installations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,20 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import effectPlugin from "@effect/eslint-plugin";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    plugins: {
+      "@effect": effectPlugin,
+    },
+    rules: {
+      // Add specific Effect rules here if desired
+      "@effect/durable-deferred": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
